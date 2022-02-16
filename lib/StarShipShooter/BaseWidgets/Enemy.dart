9c795c1d32867e3eab83081fb3aa4e5894f72f85
Awais Amin <81298743+ASquare010@users.noreply.github.com>
2022-02-16 08:15:05 +0500

    correctiom

--- lib/StarShipShooter/BaseWidgets/Enemy.dart
+++ lib/StarShipShooter/BaseWidgets/Enemy.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/StarShipShooter/NotifyListeners/EnemyDyDxNotifier.dart';
+import 'package:provider/provider.dart';
 
 class Enemy extends StatelessWidget {
   final Color colors;
 
-  const Enemy({Key key, this.colors = Colors.red}) : super(key: key);
+  const Enemy({Key key, this.colors = Colors.amber}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    EnemyDyDxNotifier _dYdX =
-        EnemyDyDxNotifier(screenWidth: MediaQuery.of(context).size.width);
+    final EnemyDyDxNotifier loc = Provider.of<EnemyDyDxNotifier>(context);
 
     return Align(
-      alignment: Alignment(_dYdX.x, _dYdX.y),
+      alignment: Alignment(loc.x, loc.y),
       child: AnimatedContainer(
-        width: _dYdX.getSize,
-        height: _dYdX.getSize,
+        width: loc.getSize,
+        height: loc.getSize,
         duration: Duration(milliseconds: 200),
         decoration: BoxDecoration(
           shape: BoxShape.circle,
