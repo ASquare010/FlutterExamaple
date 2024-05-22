001dc5eef4e4553319c7c5bad7cb9eb06aeabffa
Awais Amin <81298743+ASquare010@users.noreply.github.com>
2024-05-22 22:40:25 +0500

    Update Utils.dart
    removed water key

--- lib/WeatherForecast/Utils.dart
+++ lib/WeatherForecast/Utils.dart
@@ -1,14 +1,14 @@
 //http://api.openweathermap.org/data/2.5/forecast?q=lahore&cnt=7&units=metric&appid=d7ce83841d3908eb27a25017a80a0875link
-//d7ce83841d3908eb27a25017a80a0875 key
+
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 
 class Utils {
-  static String weatherApiKey = 'd7ce83841d3908eb27a25017a80a0875';
+  static String weatherApiKey = '***********************************';
 
-  static String googleMapApiKey = 'AIzaSyDE_roPJmvFqd9TSn2PS7kj4YT_BKZZ03I';
+  static String googleMapApiKey = '*********************************';
 
   static String getDateConverter(DateTime dateTime) {
     return DateFormat("EEE, MMM d, y").format(dateTime);
